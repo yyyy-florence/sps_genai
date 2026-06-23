e876470cdc998404b121cd1b456bbfc43c3e491e
yyyy-florence <yyuanflorence@gmail.com>
2026-06-23 12:19:09 +0800

    Add Assignment CNN architecture for Part 1

--- app/cnn_model.py
+++ app/cnn_model.py
@@ -37,3 +37,21 @@ class EnhancedCNN(nn.Module):
         x = self.dropout(x)
         x = self.fc2(x)
         return x
+
+class CNN(nn.Module):
+    """Part 1: CNN architecture as specified in Assignment 2"""
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * 16 * 16, 100)
+        self.fc2 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # 64->32
+        x = self.pool(F.relu(self.conv2(x)))  # 32->16
+        x = x.view(-1, 32 * 16 * 16)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
